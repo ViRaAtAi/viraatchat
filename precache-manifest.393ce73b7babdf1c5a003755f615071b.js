2ae21a28142df1b55b5fdf70cdc4a2726306cad2
ViRaAtAi <azototech@gmail.com>
2021-02-09 18:19:03 +0530

    Updates

---
+++ precache-manifest.393ce73b7babdf1c5a003755f615071b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f1b37125bb8323554b4a740946432d",
+    "url": "/viraatchat/index.html"
+  },
+  {
+    "revision": "c5358bdee058132e9fa6",
+    "url": "/viraatchat/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "5463e27ee34da7cc9d15",
+    "url": "/viraatchat/static/css/3.a29b55ea.chunk.css"
+  },
+  {
+    "revision": "1444b357c8babf5dbe6a",
+    "url": "/viraatchat/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "17e3746f8386aa24edc4",
+    "url": "/viraatchat/static/css/7.6fa1e2f7.chunk.css"
+  },
+  {
+    "revision": "1235669a6c10f7f58fa3",
+    "url": "/viraatchat/static/css/main.505b9513.chunk.css"
+  },
+  {
+    "revision": "c5358bdee058132e9fa6",
+    "url": "/viraatchat/static/js/0.da7df32b.chunk.js"
+  },
+  {
+    "revision": "5463e27ee34da7cc9d15",
+    "url": "/viraatchat/static/js/3.8636812a.chunk.js"
+  },
+  {
+    "revision": "661038a5c4bd811286bb",
+    "url": "/viraatchat/static/js/4.0ae9d0da.chunk.js"
+  },
+  {
+    "revision": "1444b357c8babf5dbe6a",
+    "url": "/viraatchat/static/js/5.5df8564e.chunk.js"
+  },
+  {
+    "revision": "834869b1495e8b518632",
+    "url": "/viraatchat/static/js/6.5eea57f2.chunk.js"
+  },
+  {
+    "revision": "17e3746f8386aa24edc4",
+    "url": "/viraatchat/static/js/7.1d15a0a7.chunk.js"
+  },
+  {
+    "revision": "1235669a6c10f7f58fa3",
+    "url": "/viraatchat/static/js/main.31111918.chunk.js"
+  },
+  {
+    "revision": "baf0e5a278e78ab68b5a",
+    "url": "/viraatchat/static/js/runtime~main.f9bf83c6.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/viraatchat/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/viraatchat/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/viraatchat/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/viraatchat/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
