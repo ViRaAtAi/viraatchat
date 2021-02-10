5f30a419ce518948141d0a6b9725ac30c85ec5b2
ViRaAtAi <azototech@gmail.com>
2021-02-10 12:22:36 +0530

    Updates

--- precache-manifest.f4c91c577fbd47c9328eeda6055b04c9.js
+++ precache-manifest.f4c91c577fbd47c9328eeda6055b04c9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e9fac367b6a1a86b3c345d54ea8649f0",
+    "revision": "d56cbb9fe504187f072e131e41adf902",
     "url": "/viraatchat/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/viraatchat/static/css/0.52c23fd6.chunk.css"
   },
   {
-    "revision": "df497d8e9989de70ec9c",
-    "url": "/viraatchat/static/css/3.6182e761.chunk.css"
+    "revision": "0560b8b76ae375958875",
+    "url": "/viraatchat/static/css/3.495a66ae.chunk.css"
   },
   {
     "revision": "1444b357c8babf5dbe6a",
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/viraatchat/static/js/0.da7df32b.chunk.js"
   },
   {
-    "revision": "df497d8e9989de70ec9c",
+    "revision": "0560b8b76ae375958875",
     "url": "/viraatchat/static/js/3.8636812a.chunk.js"
   },
   {
@@ -52,16 +52,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/viraatchat/static/js/main.9aaa8b0b.chunk.js"
   },
   {
-    "revision": "3a5991d26a67a4a5ad01",
-    "url": "/viraatchat/static/js/runtime~main.b8ec54db.js"
+    "revision": "e945ed80201700eae182",
+    "url": "/viraatchat/static/js/runtime~main.21845ef6.js"
   },
   {
     "revision": "85479cc199de7ace1a44d901720601b8",
     "url": "/viraatchat/static/media/bubble-tail-left.85479cc1.svg"
   },
   {
-    "revision": "fcdc50f8cc317c3afe2e27453f386e28",
-    "url": "/viraatchat/static/media/camomile_blurred.fcdc50f8.jpg"
+    "revision": "67ed9fd696d571bca0c586eca3a792b8",
+    "url": "/viraatchat/static/media/camomile_blurred.67ed9fd6.jpg"
   },
   {
     "revision": "71da74694e314a0e3a855ded564a1eaf",
